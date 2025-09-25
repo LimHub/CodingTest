c470faa21668683185c81f6ff376551da2ef0070
LimHub <115395940+LimHub@users.noreply.github.com>
2025-09-25 11:23:01 +0900

    [Bronze I] Title: 평균, Time: 224 ms, Memory: 19164 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int[] arr = new int[N];
+    int max = 0;
+    int sum = 0;
+    for(int i = 0 ; i < N ; i++){
+      arr[i] = sc.nextInt();
+      if(arr[i]>  max){
+        max = arr[i];
+      }
+      sum +=arr[i];
+    }
+    System.out.println(sum* 100.0/max/ N);
+  }
+}
