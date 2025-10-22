8728246b79b887cb81cbf351ee2c33cbdbc74c4b
LimHub <115395940+LimHub@users.noreply.github.com>
2025-10-22 16:13:27 +0900

    [Gold I] Title: 구간 합 구하기, Time: 528 ms, Memory: 116464 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,54 +1,76 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+  static long[] arr;
+  static long[] tree;
   public static void main(String args[])throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     StringTokenizer st = new StringTokenizer(br.readLine());
 
     int N = Integer.parseInt(st.nextToken());
     int M = Integer.parseInt(st.nextToken());
     int K = Integer.parseInt(st.nextToken());
 
-    long[] arr = new long[N];
- 
-
-    for(int i = 0 ; i < N ; i++){
+    arr = new long[N+1];
+    for (int i = 1; i <= N; i++) {
       arr[i] = Long.parseLong(br.readLine());
     }
-    long[] sumArr = new long [N];
-    sumArr[0] = arr[0];
-    for(int i = 1 ; i < N ; i++){
-      sumArr[i] = sumArr[i-1] + arr [i];
-    }
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    tree = new long[4 * N];
+
+    init(1,N,1);
     StringBuilder sb = new StringBuilder();
-    long answer;
-    long gap;
     for(int i = 0 ; i < M + K ; i++){
       st = new StringTokenizer(br.readLine());
       int a = Integer.parseInt(st.nextToken());
       int b = Integer.parseInt(st.nextToken());
-      long c = Long.parseLong(st.nextToken());
-      if(a == 1 ){
-        gap = c - arr[b - 1];
-        arr[b-1] =c;
-        for(int j = b - 1 ; j < N ; j++){
-          sumArr[j] += gap;
-        }
-      }
-      else if(a == 2){
-        if(b == 1){
-          answer = sumArr[(int)c-1];
-          sb.append(answer).append("\n");
-      }else{
-          answer = sumArr[(int)c-1] - sumArr[b-2];
-          sb.append(answer).append("\n");
-      }
+
+      if(a == 1){
+        long c = Long.parseLong(st.nextToken());
+        long diff = c - arr[b];
+        arr[b] = c;
+        update(1, N ,1 , b ,diff);
+      }else if(a == 2){
+        int c = Integer.parseInt(st.nextToken());
+        sb.append(sum(1, N, 1, b, c)).append("\n");
       }
     }
     bw.write(sb.toString());
     bw.close();
 
   }
+  public static long init(int start , int end , int node){
+    if(start == end){
+      return tree[node] = arr[start];
+    }
+    int mid = ( start + end ) / 2 ;
+    return tree[node] = init(start , mid , node * 2) + init(mid + 1 , end , node * 2 +1);
+  }
+
+  public static long sum(int start , int end , int node, int left , int right){
+    if(left > end || right < start){
+      return 0;
+    }
+    if(left <= start && end <= right){
+      return tree[node];
+    }
+    int mid = ( start + end ) / 2 ;
+    return sum(start , mid , node * 2 , left , right) + sum(mid + 1 , end , node * 2 + 1, left , right);
+  }
+
+  public static void update( int start , int end, int node , int index , long diff){
+    if(index < start || index > end ){
+      return;
+    }
+    tree[node] += diff;
+    if(start != end){
+      int mid = ( start + end ) / 2 ;
+      update(start, mid, node * 2, index, diff);
+      update(mid + 1, end, node * 2 + 1, index, diff);
+    }
+  }
 }
+
+
