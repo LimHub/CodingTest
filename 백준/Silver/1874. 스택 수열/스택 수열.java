d57e7d0fb97da37438feba22862a6a499688a5bf
LimHub <115395940+LimHub@users.noreply.github.com>
2025-12-01 12:50:17 +0900

    [Silver II] Title: 스택 수열, Time: 876 ms, Memory: 111780 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -3,41 +3,36 @@ import java.io.*;
 
 public class Main {
   public static void main(String args[]){
-    Scanner sc  = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
     int N = sc.nextInt();
-    if(N< 1 || N > 100000){
-      System.out.println("주어진 범위를 다시 확인해주세요.");
-      return;
-    }
-    int[] answer =  new int[N];
-    for(int i = 0; i < N ; i++){
+    int[] answer = new int[N];
+    for(int i = 0 ; i < N ; i++){
       answer[i] = sc.nextInt();
     }
-    Stack<Integer> stack = new Stack<>();
-    StringBuffer bf = new StringBuffer();
     int num = 1;
+    Stack<Integer> stack = new Stack<>();
+    StringBuffer sb = new StringBuffer();
     boolean result = true;
-    for(int i =0 ; i < N ; i++){
-      int su = answer[i];
-      if(su >= num){
-        while(su>=num){
-          stack.push(num++);
-          bf.append("+\n");
+    for(int i = 0 ; i < N ; i++){
+        int index = answer[i];
+        if(index >= num){
+          while(index >= num){
+            stack.push(num++);
+            sb.append("+\n");
+          }
+          stack.pop();
+          sb.append("-\n");
         }
-        stack.pop();
-        bf.append("-\n");
-      }
-      else{
-        int n = stack.pop();
-        if(n > su){
-          System.out.println("NO");
-          result = false;
-          break;
+        else{
+          int n = stack.pop();
+          if(n > index){
+            System.out.println("NO");
+            result = false;
+            break;
+          }
+          sb.append("-\n");
         }
-        bf.append("-\n");
-      }
-      
     }
-    if(result) System.out.println(bf.toString());
+    if(result) System.out.println(sb.toString());
   }
 }
